944258ce968cb359f63b4a4ed053410e5eef5901
brett <brett.prescott17@gmail.com>
2023-10-22 03:48:03 -0400

    Functional resize button
    User input from resize prompt as input to resize function. This
    removes the old grid and creates a new one abiding by new sizing. Used
    inline CSS for consistent sizing after finding flex-grow inconsistently
    sized internal elements leaving the bottom row of divs in the container
    larger than the rest to absorb the remainder of available space.

--- javascript.js
+++ javascript.js
@@ -7,37 +7,51 @@ let resize = document.createElement('button');
 resize.textContent = 'Resize';
 
 body.appendChild(resize);
+body.appendChild(container)
 
-
-let size = 256;
-/* Generate a 16x16 grid of divs within container */
-let createGrid = (size) => {
-    body.appendChild(container);
-    for (let i = 1; i < size + 1; i++) {
-    let subCon = document.createElement('div');
-    subCon.className = 'subcon';
-    subCon.setAttribute('id', i)
-    container.appendChild(subCon)
-    }
-}
-createGrid(size);
 /* Generate a random color */
+
 const randColor = () =>  {
     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
+
+/* Generate a grid of divs within container based on size parameters */
+
+let createGrid = () => {
+    for (let i = 1; i < size + 1; i++) {
+    let subCon = document.createElement('div');
+    subCon.className = 'subcon';
+    subCon.setAttribute('id', i);
+    subCon.style.width = (800 / Math.sqrt(size)) + 'px';
+    container.appendChild(subCon);
+    console.log(size);
+    }
+    let squares = document.querySelectorAll('.subcon')
+    for (const square of squares) {
+        square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = randColor();
+    })
+    }
+}
+/* Apply initial grid */
+
+let size = 256;
+createGrid(size);
+
+
+/* Removes all child nodes of a container */
+
+remChildren = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+/* Creates a new grid based off input size returned */
+
 resize.addEventListener('click', () => {
     let newSize = prompt('Enter a whole number of squares per side between 1 and 100 for a new grid');
-    console.log(newSize)
-    if (0 < newSize < 100) {
-        size = newSize * newSize;
-        body.removeChild(container);
-        createGrid(size);
-    }
+    size = newSize * newSize;
+    remChildren(container);
+    createGrid(size);
 })
-const squares = document.querySelectorAll('.subcon')
-for (const square of squares) {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = randColor();
-        console.log(square.backgroundColor);
-    })
-}
+
