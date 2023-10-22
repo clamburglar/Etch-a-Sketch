1e899f634d46b2383b172dd9ffd822f7cb66db4c
brett <brett.prescott17@gmail.com>
2023-10-21 23:30:58 -0400

    Etch a sketch now functional
    Added listeners to all squares in the grid which now change to a
    randomly generated color when mouseover. Listener added to the resize
    button to collect input to use as grid size parameters.

--- javascript.js
+++ javascript.js
@@ -7,25 +7,37 @@ let resize = document.createElement('button');
 resize.textContent = 'Resize';
 
 body.appendChild(resize);
-body.appendChild(container);
 
+
+let size = 256;
 /* Generate a 16x16 grid of divs within container */
-for (let i = 1; i < 257; i++) {
+let createGrid = (size) => {
+    body.appendChild(container);
+    for (let i = 1; i < size + 1; i++) {
     let subCon = document.createElement('div');
     subCon.className = 'subcon';
     subCon.setAttribute('id', i)
     container.appendChild(subCon)
+    }
 }
-
+createGrid(size);
 /* Generate a random color */
 const randColor = () =>  {
     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
-
-const buttons = document.querySelectorAll('.subcon')
-for (const button of buttons) {
-    button.addEventListener('mouseenter', (event) => {
-        event.button.style.color = randColor();
-        console.log(button.style.color);
+resize.addEventListener('click', () => {
+    let newSize = prompt('Enter a whole number of squares per side between 1 and 100 for a new grid');
+    console.log(newSize)
+    if (0 < newSize < 100) {
+        size = newSize * newSize;
+        body.removeChild(container);
+        createGrid(size);
+    }
+})
+const squares = document.querySelectorAll('.subcon')
+for (const square of squares) {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = randColor();
+        console.log(square.backgroundColor);
     })
 }
